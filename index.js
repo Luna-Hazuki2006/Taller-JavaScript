7a28193aa3f512541935188e2c56047665d4b8f8
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-15 18:27:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const registrar = document.getElementById("registrar")
+
+if (registrar) {
+    registrar.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const nombres = document.getElementById("nombres")
+    })
+}

c4eab35fe1be4fa9df5de547e1ccec5cf785fed8
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-16 09:23:10 -0400

    algunos

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ if (registrar) {
     registrar.addEventListener("submit", (event) => {
         event.preventDefault()
         const nombres = document.getElementById("nombres")
-        
+        const apellidos = document.getElementById("apellidos")
+        const email = document.getElementById("email")
+        const contraseña = document.getElementById("contraseña")
+        if (nombres.value === "" || apellidos.value === "" || email.value === "" || contraseña.value === "") {
+            Swal.fire({
+                icon: 'error',
+                title: '¡Oh no!',
+                text: 'No se puede registrar si '
+            })
+        }
+
     })
 }

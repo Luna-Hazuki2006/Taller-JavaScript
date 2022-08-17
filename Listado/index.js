188cd162ce73b76e4dce13cf01644ecedd8520bd
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-17 14:18:52 -0400

    aaaaa

--- Listado/index.js
+++ Listado/index.js
@@ -1,14 +1,17 @@
 // aqui va todo el código
 console.log("hola mundo")
 
-const dar_data = async (url) => {
+const dar_data = async (url, method = "GET", body = null) => {
     try {
         const token = localStorage.getItem("token")
+        !token && ( window.location.href = "/" )
         const response = await fetch(url, {
             headers: {
+                'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
-            },
-            body: null
+            }, 
+            method: method,
+            body: body
         })
         // transformar a JSON
         const data = await response.json()
@@ -22,7 +25,7 @@ const dar_data = async (url) => {
     }
 }
 
-const init = async () => {
+const llenar = async () => {
     try {
         // llamar api
         const a = await dar_data("https://graco-task-list.herokuapp.com/task")
@@ -61,7 +64,7 @@ const init = async () => {
                     
                 </div>
                 <span class="badge bg-primary rounded-pill mx-1">${element.priority}</span>
-                <button onclick="deleteTask(${element._id})" type="button" class="btn btn-outline-danger btn-sm">
+                <button onclick="deleteTask('${element._id}')" type="button" class="btn btn-outline-danger btn-sm">
                     <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px"
                         height="20px">
                         <path
@@ -71,12 +74,14 @@ const init = async () => {
             </li>
             `
         });
+        if (data.data.length > 0) boton.style.display = "block"; else boton.style.display = "none"
+        dar_numeros()
     } catch (error) {
         console.error(error)
         console.log("hubo un error");
     }
 }
-init()
+llenar()
 
 const boton = document.getElementById("boton-borrar")
 
@@ -94,22 +99,31 @@ const dar_numeros = async () => {
     }
 }
 
-dar_numeros()
-
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
     console.log("eliminando....", id)
     // buscamos etiqueta ol
-    const taskListElement = document.getElementById("task-list")
     // buscamos etiqueta li por el id
-    const elementToDelete = document.getElementById(id)
-
+    // const elementToDelete = document.getElementById(id)
+    const a = await dar_data(`https://graco-task-list.herokuapp.com/task/${id}`, "DELETE")
     // eliminamos etiqueta li
-    taskListElement.removeChild(elementToDelete)
-
-    if (taskListElement.children.length == 0) {
+    // taskListElement.removeChild(elementToDelete)
+    if (a.response.status !== 200) {
+        Swal.fire({
+            icon: 'error',
+            title: '¡Oh no!',
+            text: '¡No funcionó!'
+        })
+        return
+    }
+    Swal.fire(
+        '¡Funcionó!',
+        '¡Tu tarea ha sido eliminada!',
+        'success'
+    )
+    if (a.data.data.length == 0) {
         boton.style.display = "none"
     } else boton.style.display = "block"
-    dar_numeros()
+    llenar()
 }
 
 // codigo de ejemplo
@@ -124,22 +138,31 @@ if (btnElement) {
 
 if (boton) {
     boton.addEventListener("click", () => {
-        if (confirm("¿Está seguro que quiere eliminar todas las tareas?") === true) {
-            console.log("Eliminando tareas");
-            const audio = new Audio("assets/Man falls down stairs meme.mp3")
-            audio.play()
-            const taskListElement = document.getElementById("task-list")
-            while (taskListElement.children.length > 0) {
-                for (let i = 0; i < taskListElement.children.length; i++) {
-                    let element = taskListElement.children[i];
-                    taskListElement.removeChild(element)
-                }   
-            }
-            dar_numeros()
-            boton.style.display = "none"
-            console.log(taskListElement.children.length);
-            console.log("se eliminaron")            
-        } else boton.style.display = "block"
+        Swal.fire({
+            title: '¿Está seguro que quiere eliminar todas las tareas?',
+            text: "¡No serás capaz de recuperarlas?",
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, ¡Bórralo todo!'
+            }).then(async (result) => {
+            if (result.isConfirmed) {
+                console.log("Eliminando tareas");
+                const audio = new Audio("assets/Man falls down stairs meme.mp3")
+                audio.play()
+                const a = await dar_data("https://graco-task-list.herokuapp.com/task/delete/all")
+                if (a.response.status === 200) {
+                    Swal.fire(
+                        '¡Funcionó!',
+                        '¡Todo se borró!',
+                        'success'
+                    )
+                    boton.style.display = "none"
+                    console.log("se eliminaron")
+                }
+            } else boton.style.display = "block"
+        })
     })
     dar_numeros()
 }
@@ -150,87 +173,42 @@ const formElement = document.getElementById("task-form")
 if (formElement) {
     console.log("formulario funcionando...")
     // modificar el evento
-    formElement.addEventListener("submit", function(event) {
+    formElement.addEventListener("submit", async function(event) {
         event.preventDefault()
-       
         // extraer los datos nombre de la tarea y prioridad
         const inputElement = document.getElementById("taskName")
-
         const inputDate = document.getElementById("taskDate")
-
+        const selectElement = document.getElementById("taskPriority")
         console.log(inputDate.value);
         if (inputElement.value !== "" && inputDate.value !== "") {
-            // buscar el select
-            const selectElement = document.getElementById("taskPriority")
-            let fondo = 'background-color: '
-            switch (selectElement.value) {
-                case "1":
-                case "2":
-                    fondo += 'blue; color: white;'
-                    break;
-                case "3":
-                case "4":
-                    fondo += 'yellow;'
-                    break;
-                case "5":
-                    fondo += 'red; color: white;'
-                default:
-                    break;
+            const objeto = {
+                'name': inputElement.value, 
+                'date': inputDate.value, 
+                'priority': parseInt(selectElement.value)
             }
-            console.log(fondo);
-            const fecha = new Date(inputDate.value)
-            const dia = fecha.getDate() + 1
-            const mes = fecha.getMonth() + 1
-            const fecha_real = `${(dia <= 9) ? "0" + dia : dia}/${(mes <= 9) ? "0" + mes : mes}/${fecha.getFullYear()}`
-            
-            // tengo los valoes del input y del select
-            console.log( inputElement.value )
-            console.log( selectElement.value )
-            console.log( inputDate.value );
-
-            // agregar nuevo li a la lista
-            const taskListElement = document.getElementById("task-list")
-
-            let mayorId = 1 
-
-            // verificamos cantida de hijos de una etiqueta
-            if (taskListElement.children.length > 0) {
-                // buscamos el hijo
-                const ultimoHijo = taskListElement.children[ taskListElement.children.length - 1 ]
-                // incrementar
-                mayorId += parseInt( ultimoHijo.id ) 
-            } 
-
-            taskListElement.innerHTML += `
-            <li id="${mayorId}" class="list-group-item d-flex justify-content-between align-items-center"
-                style="word-break: keep-all; ${fondo}">
-                <div class="mx-2 text-start" style="flex: 1;">
-                    <div class="fw-bold">${inputElement.value}</div>
-                    <div>${fecha_real}</div>
-                </div>
-                    
-                </div>
-                <span class="badge bg-primary rounded-pill mx-1">${selectElement.value}</span>
-                <button onclick="deleteTask(${mayorId})" type="button" class="btn btn-outline-danger btn-sm">
-                    <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px"
-                        height="20px">
-                        <path
-                            d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z" />
-                    </svg>
-                </button>
-            </li>
-            `
-
-            if (taskListElement.children.length > 0) {
-                boton.style.display = "block"
-            } else {
-                boton.style.display = "none"
+            const a = await dar_data("https://graco-task-list.herokuapp.com/task", 'POST', JSON.stringify(objeto))
+            if (a.response.status !== 200) {
+                Swal.fire({
+                    icon: 'error',
+                    title: '¡Oh no!',
+                    text: '¡No funcionó!'
+                })
+                return
             }
-
-            dar_numeros()
+            Swal.fire({
+                icon: 'succes',
+                title: '¡Funcionó!',
+                text: '¡Se agregó una nueva tarea!'
+            })
+            if (taskListElement.children.length > 0) boton.style.display = "block"; else boton.style.display = "none"
+            llenar()
 
         } else {
-            alert(" Debes especificar una tarea prro >:v ")
+            Swal.fire({
+                icon: 'error',
+                title: '¡Así no se hace >:V!',
+                text: 'Debes especificar una tarea prro >:v'
+            })
         }
 
     })
